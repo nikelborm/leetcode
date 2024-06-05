156cfe99d59893026ecd510c81d54b81abaf7fc3
nikelborm <evadev@duck.com>
2024-06-05 22:24:33 +0300

    save

---
+++ nikelborm/0009-palindrome-number/index.ts
@@ -0,0 +1,15 @@
+const getDigit = (x: number, pos: number) => Math.floor(x / 10 ** pos) % 10;
+const getDigitAmount = (x: number) => Math.ceil(Math.log10(x + 1));
+
+function isPalindrome(x: number): boolean {
+  if (x < 10 && x >= 0) return true;
+  if ((x % 10 === 0 && x > 0) || x < 0) return false;
+
+  const digitAmount = getDigitAmount(x);
+
+  for (let i = 0; i < digitAmount / 2; i++) {
+    console.log(i);
+    if (getDigit(x, i) !== getDigit(x, digitAmount - i - 1)) return false;
+  }
+  return true;
+}
---
+++ nikelborm/0203-remove-linked-list-elements/index.ts
@@ -0,0 +1,31 @@
+interface ListNode {
+  val: number;
+  next: ListNode | null;
+}
+
+function removeElements(
+  initialHead: ListNode | null,
+  numToExclude: number
+): ListNode | null {
+  let newHead = initialHead;
+  while (newHead?.val === numToExclude) {
+      newHead = newHead.next;
+  }
+
+  if (!newHead?.next) return newHead;
+
+  let previous: ListNode = newHead;
+  let current: ListNode = newHead.next;
+
+  while (current.next) {
+      if (current.val === numToExclude) previous.next = current.next;
+      else previous = previous.next as ListNode;
+      current = current.next;
+  }
+
+  if (current.val === numToExclude) {
+      previous.next = null;
+  }
+
+  return newHead;
+}
