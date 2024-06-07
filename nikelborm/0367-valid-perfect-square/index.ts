b497e682798976724e6425e23382926b2d5f87b6
nikelborm <kolya007.klass@gmail.com>
2024-06-07 18:29:21 +0300

    save

---
+++ nikelborm/0367-valid-perfect-square/index.ts
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/valid-perfect-square/description/
+
+import { deepEqual, equal } from 'assert';
+import { it } from 'node:test';
+
+function isPerfectSquare(num: number): boolean {
+  while(false) {
+
+  }
+
+  return false;
+};
+
+it('case 1', () => {
+  equal(isPerfectSquare(16), true);
+})
+
+it('case 2', () => {
+  equal(isPerfectSquare(16), true);
+})
