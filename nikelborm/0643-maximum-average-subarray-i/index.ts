328b0c8a8016dda2037fbe127aa6707531c79eb9
nikelborm <kolya007.klass@gmail.com>
2024-06-07 14:49:42 +0300

    save

---
+++ nikelborm/0643-maximum-average-subarray-i/index.ts
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/maximum-average-subarray-i/description/
+
+import { deepEqual, equal } from 'assert';
+import { it } from 'node:test';
+
+function findMaxAverage(numbers: number[], k: number): number {
+  let currentSum = numbers.slice(0, k).reduce((acc, cur) => acc + cur, 0);
+  let bestSum = currentSum;
+  for (let index = k; index < numbers.length; index++) {
+    currentSum = currentSum + numbers[index] - numbers[index - k]
+    if(currentSum > bestSum)
+      bestSum = currentSum
+  }
+
+  return bestSum / k;
+};
+
+it('case 1', () => {
+  equal(findMaxAverage([1, 12, -5, -6, 50, 3], 4), 12.75000)
+})
+
+it('case 2', () => {
+  equal(findMaxAverage([5], 1), 5)
+})
